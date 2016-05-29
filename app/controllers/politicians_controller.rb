637d8226cc4f17f1c9bc22d4894beb288bc35b62
Bejan Maljai <bmaljai@gmail.com>
2016-05-28 20:01:39 -0700

    initial commit

---
+++ app/controllers/politicians_controller.rb
@@ -0,0 +1,10 @@
+class PoliticiansController < ApplicationController
+  def index
+    @politicians = Unirest.get("localhost:3000/api/v1/politicians.json").body
+  end
+
+  def show
+    @politician = Unirest.get("localhost:3000/api/v1/politicians/#{params[:id].json}").body
+  end
+
+end

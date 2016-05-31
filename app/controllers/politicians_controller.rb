e61b140e197f4d8b5cd98ddbbb9d8c74e5e4195f
Bejan Maljai <bmaljai@gmail.com>
2016-05-30 17:34:35 -0700

    finished restful actions

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -24,11 +24,25 @@ class PoliticiansController < ApplicationController
   end
 
   def edit
+    @politician = Unirest.get("#{ENV['DOMAIN']}/politicians/#{params[:id]}.json").body
   end
 
   def update
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    birthdate = params[:birthdate]
+    total_years_service = params[:total_years_service]
+    party = params[:party]
+    current_office = params[:current_office]
+
+    @politician = Unirest.patch("#{ENV['DOMAIN']}/politicians/#{params[:id]}.json", headers:{"Accept"=>"application/json"}, parameters:{first_name: first_name, last_name: last_name, birthdate: birthdate, total_years_service: total_years_service, party: party, current_office: current_office}).body
+
+    redirect_to "/politicians/#{@politician["id"]}"
   end
 
   def destroy
+    Unirest.delete("#{ENV['DOMAIN']}/politicians/#{params[:id]}")
+
+    redirect_to "/politicians"
   end
 end
